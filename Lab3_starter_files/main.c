024d644ef78f2f65c0e5ac1f005f65be469b07b3
RManu1 <87384244+RManu1@users.noreply.github.com>
2022-02-09 10:57:24 -0800

    Update main.c

--- Lab3_starter_files/main.c
+++ Lab3_starter_files/main.c
@@ -1,34 +1,34 @@
 //*****************************************************************************
 //
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
+// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/
+//
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
 //  are met:
 //
-//    Redistributions of source code must retain the above copyright 
+//    Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
 //
 //    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the
 //    distribution.
 //
 //    Neither the name of Texas Instruments Incorporated nor the names of
 //    its contributors may be used to endorse or promote products derived
 //    from this software without specific prior written permission.
 //
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 //  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
 //  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 //  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //*****************************************************************************
@@ -36,8 +36,8 @@
 //*****************************************************************************
 //
 // Application Name     - SPI Demo
-// Application Overview - The demo application focuses on showing the required 
-//                        initialization sequence to enable the CC3200 SPI 
+// Application Overview - The demo application focuses on showing the required
+//                        initialization sequence to enable the CC3200 SPI
 //                        module in full duplex 4-wire master and slave mode(s).
 //
 //*****************************************************************************
@@ -384,17 +384,18 @@ void writeCommand(unsigned char c) {
 *  SPI.
 *
 */
-    unsigned long ulDummy;
+      unsigned long ulDummy;
 
-    MAP_SPITransfer(GSPI_BASE,g_ucTxBuff,g_ucRxBuff,50,
-                SPI_CS_ENABLE|SPI_CS_DISABLE);
-    MAP_SPICSEnable(GSPI_BASE);
-    GPIOPinWrite(GPIOA3_BASE,0x2,0x00);    // Set DC Low
-    GPIOPinWrite(GPIOA3_BASE,0x80,0x00);   // Set CS Low
+    /*  MAP_SPITransfer(GSPI_BASE,g_ucTxBuff,g_ucRxBuff,50,
+                  SPI_CS_ENABLE|SPI_CS_DISABLE);
+      MAP_SPICSEnable(GSPI_BASE);
+      */
+      GPIOPinWrite(GPIOA3_BASE,0x10,0);    // Set DC Low
+      GPIOPinWrite(GPIOA3_BASE,0x80,0);   // Set CS Low
 
-    MAP_SPIDataPut(GSPI_BASE,c);        // Send c to OLED
-    MAP_SPIDataGet(GSPI_BASE,&ulDummy);
-    MAP_SPICSDisable(GSPI_BASE);
+      MAP_SPIDataPut(GSPI_BASE,c);        // Send c to OLED
+      MAP_SPIDataGet(GSPI_BASE,&ulDummy);
+     // MAP_SPICSDisable(GSPI_BASE);
 
 }
 //*****************************************************************************
@@ -407,15 +408,16 @@ void writeData(unsigned char c) {
 */
     unsigned long ulDummy;
 
-    MAP_SPITransfer(GSPI_BASE,g_ucTxBuff,g_ucRxBuff,50,
+  /*  MAP_SPITransfer(GSPI_BASE,g_ucTxBuff,g_ucRxBuff,50,
                 SPI_CS_ENABLE|SPI_CS_DISABLE);
     MAP_SPICSEnable(GSPI_BASE);
-    GPIOPinWrite(GPIOA3_BASE,0x2,0xFF);    // Set DC High
-    GPIOPinWrite(GPIOA3_BASE,0x80,0x00);   // Set CS Low
+    */
+    GPIOPinWrite(GPIOA3_BASE,0x10,0xFF);    // Set DC High
+    GPIOPinWrite(GPIOA3_BASE,0x80,0);   // Set CS Low
 
     MAP_SPIDataPut(GSPI_BASE,c);        // Send c to OLED
     MAP_SPIDataGet(GSPI_BASE,&ulDummy);
-    MAP_SPICSDisable(GSPI_BASE);
+   // MAP_SPICSDisable(GSPI_BASE);
 }
 
 //*****************************************************************************
@@ -707,11 +709,16 @@ void main()
 
 
 
+
+                  MAP_SPITransfer(GSPI_BASE,g_ucTxBuff,g_ucRxBuff,50,
+                              SPI_CS_ENABLE|SPI_CS_DISABLE);
+                  MAP_SPICSEnable(GSPI_BASE);
+
+
                 Adafruit_Init();
     //write to RAM
      /*
                 writeCommand(SSD1351_CMD_WRITERAM);
-
                 unsigned int i;
               for(i = 0; i < sizeof(font); i++)
                 {
@@ -721,7 +728,7 @@ void main()
         writeCommand(SSD1351_CMD_DISPLAYOFF);
         MAP_UtilsDelay(8000000);
         writeCommand(SSD1351_CMD_DISPLAYALLOFF);
-        */
+
         writeCommand(SSD1351_CMD_WRITERAM);
 
         writeData(0x48);
@@ -735,6 +742,18 @@ void main()
         writeData(0x52);
         writeData(0x4C);
         writeData(0x44);
+        */
             writeCommand(SSD1351_CMD_DISPLAYOFF);
 
+             writeCommand(SSD1351_CMD_DISPLAYALLON);
+             MAP_UtilsDelay(9000000);
+
+             writeCommand(SSD1351_CMD_DISPLAYALLOFF);
+            MAP_SPICSDisable(GSPI_BASE);
+
+
+   while(1)
+   {}
+
 }
+
